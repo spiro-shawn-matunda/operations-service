615560fa39d915036a8cbeb111f8363e88aa84e9
Cynthia Situma <cynthia.situma@spironet.com>
2025-09-11 11:18:29 +0300

    adding model

---
+++ src/main/java/com/spiro/operation/model/shifttable.java
@@ -0,0 +1,4 @@
+package com.spiro.operation.model;
+
+public class shifttable {
+}
---
+++ src/main/java/com/spiro/operation/model/station.java
@@ -0,0 +1,12 @@
+package com.spiro.operation.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class station {
+
+
+
+
+
+}
