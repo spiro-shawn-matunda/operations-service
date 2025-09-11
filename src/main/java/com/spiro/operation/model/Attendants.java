473c70ac6e22d55cc7dc9c56259c1253401b65c9
Shawn  Matunda <shawn.matunda@spironet.com>
2025-09-11 12:08:34 +0300

    check

--- src/main/java/com/spiro/operation/model/Attendants.java
+++ src/main/java/com/spiro/operation/model/Attendants.java
@@ -1,4 +1,76 @@
 package com.spiro.operation.model;
 
-public class attendants {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Attendants {
+
+
+    private String firstName;
+    private String secondName;
+    private int phoneNumber;
+    private int nationalId;
+    private String email;
+    private String homeAddress;
+
+    public Attendants (String firstName,String secondName,int phoneNumber,int nationalId,String email,String homeAddress){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.phoneNumber = phoneNumber;
+        this.nationalId =nationalId;
+        this.email = email ;
+        this.homeAddress = homeAddress;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getNationalId() {
+        return nationalId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setNationalId(int nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+
 }
